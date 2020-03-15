48c3a029686334d48635ab4b0a7f8ba4c4c9cea2
Alon Gamliel <alon@gamliela.com>
2020-03-15 16:22:42 +1300

    Remove HomePage component as it creates duplicate route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
-import HomePage from "./HomePage/HomePage";
+import * as React from "react";
+import style from "./style.module.scss";
+import Head from "next/head";
 
-export default HomePage;
+const Page = () => (
+  <div className={style.index}>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <h1 className={style.header}>Hello World!</h1>
+  </div>
+);
+
+export default Page;
