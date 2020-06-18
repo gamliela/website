47e86b674e625092085a29cf9d24d54773b1f1ba
Alon Gamliel <alon@gamliela.com>
2020-06-18 23:10:35 +1200

    Add working example for MDX

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+// we need this rule because eslint apply typescript rules on all files, including js files.
+// see https://github.com/Shopify/eslint-plugin-shopify/issues/159
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx"],
   target: "serverless",
   distDir: ".next",
-};
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import style from "./style.module.scss";
 import Head from "next/head";
+import Link from "next/link";
 
 const Page = () => (
   <div className={style.index}>
@@ -9,7 +10,14 @@ const Page = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <h1 className={style.header}>Hello World!</h1>
+    <h1 className={style.header}>Alon Gamliel</h1>
+    <ul>
+      <li>
+        <Link href="blog/practical-introduction-to-serverless-development">
+          Practical Introduction to Serverless Development
+        </Link>
+      </li>
+    </ul>
   </div>
 );
 
