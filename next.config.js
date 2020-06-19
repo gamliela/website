e839cb9577793fb2ee4cdd3dccd07dfa776afd8c
Alon Gamliel <alon@gamliela.com>
2020-06-20 00:20:55 +1200

    Add images

--- next.config.js
+++ next.config.js
@@ -8,8 +8,12 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  target: "serverless",
-  distDir: ".next",
-});
+const withImages = require("next-images");
+
+module.exports = withImages(
+  withMDX({
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    target: "serverless",
+    distDir: ".next",
+  })
+);
