1a00d2351feb3c77d2cea7d06ceb52314c6d1ee3
Alon Gamliel <alon@gamliela.com>
2020-07-28 22:56:49 +1200

    Apply basic layout style

--- src/components/BlogLayout.tsx
+++ src/components/BlogLayout.tsx
@@ -8,17 +8,23 @@ const components = {
 
 function withBlogLayout(Content: () => JSX.Element, metadata?: BlogMetadata) {
   function BlogLayout() {
-    const title = metadata?.title && <h1>{metadata?.title}</h1>;
-    const subTitle = metadata?.subTitle && <h2>{metadata?.subTitle}</h2>;
+    const title = metadata?.title && (
+      <h1 className="text-center">{metadata?.title}</h1>
+    );
+    const subTitle = metadata?.subTitle && (
+      <div className="lead text-center">{metadata?.subTitle}</div>
+    );
 
     return (
       <MDXProvider components={components}>
-        <article className="container mx-auto prose lg:prose-xl">
-          <header>
-            {title}
-            {subTitle}
-          </header>
-          <Content />
+        <article className="container mx-auto antialiased text-gray-900 px-6 py-6">
+          <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
+            <header>
+              {title}
+              {subTitle}
+            </header>
+            <Content />
+          </div>
         </article>
       </MDXProvider>
     );
