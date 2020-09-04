161e405f8d92f6ecc19be70217ccb1ca873bad8a
Alon Gamliel <alon@gamliela.com>
2020-09-04 23:26:56 +1200

    Add react-admin dependency

---
+++ src/types/react-admin.d.ts
@@ -0,0 +1,8 @@
+declare module "react-admin" {
+  import { FunctionComponent } from "react";
+  import { AdminProps } from "ra-core";
+
+  const Admin: FunctionComponent<AdminProps>;
+
+  export { Admin };
+}
