143651c2b67ef2c31f4a68b579b9a710be20a18c
Alon Gamliel <alon@gamliela.com>
2020-09-06 21:55:24 +1200

    Add basic typing for react-admin

--- src/types/react-admin.d.ts
+++ src/types/react-admin.d.ts
@@ -1,8 +1,45 @@
 declare module "react-admin" {
-  import { FunctionComponent } from "react";
-  import { AdminProps } from "ra-core";
+  /* eslint-disable */
+  const Admin: any;
+  const Resource: any;
+  const ListGuesser: any;
+  const List: any;
+  const Datagrid: any;
+  const TextField: any;
+  const EmailField: any;
+  const UrlField: any;
+  const ReferenceField: any;
+  const EditButton: any;
+  const EditGuesser: any;
+  const Edit: any;
+  const SimpleForm: any;
+  const TextInput: any;
+  const ReferenceInput: any;
+  const SelectInput: any;
+  const Create: any;
+  const Filter: any;
+  /* eslint-enable */
 
-  const Admin: FunctionComponent<AdminProps>;
+  export {
+    Admin,
+    ListGuesser,
+    Resource,
+    List,
+    Datagrid,
+    TextField,
+    EmailField,
+    UrlField,
+    ReferenceField,
+    EditButton,
+    EditGuesser,
+    Edit,
+    SimpleForm,
+    TextInput,
+    ReferenceInput,
+    SelectInput,
+    Create,
+    Filter,
+  };
 
-  export { Admin };
+  export * from "ra-core/src/types";
 }
